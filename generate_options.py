7cc0729bfb23d61bb4b2846f8f46596e691b640a
Morgan1420 <jan.moran.r@gmail.com>
2026-05-08 13:45:48 +0200

    generate complete options

--- generate_options.py
+++ generate_options.py
@@ -27,6 +27,15 @@ def areInputsValid(fc, bw, time, num_chan):
     # Else: if all is good
     return True, "Tots els paràmetres són vàlids."
 
+def readJSON(json_path):
+    try:
+        with open(json_path, 'r') as file:
+            data = json.load(file)
+        return data
+    except Exception as e:
+        print(f"Error: loading JSON failed: {e}")
+        return None
+
 def storeJSON(data, json_path):
     try:
         with open(json_path, 'w') as file:
@@ -46,9 +55,11 @@ def generatePartialOptions(fc, bw, mcr_converter_rates_table_path, partial_optio
     f_max_target = fc + (bw / 2)
     
     # Read converter_rates table (.json)
-    with open(mcr_converter_rates_table_path, 'r') as file:
-        mcr_converter_rates_table = json.load(file)
-     
+    
+    mcr_converter_rates_table = readJSON(mcr_converter_rates_table_path)
+    if mcr_converter_rates_table is None:
+        return []
+
     for entry in mcr_converter_rates_table:
         mcr_hz = entry["mcr_mhz"] * 1e6
         
@@ -82,6 +93,7 @@ def generatePartialOptions(fc, bw, mcr_converter_rates_table_path, partial_optio
                     chans_needed = math.ceil(interest_bw / usable_bw_per_chan)
                     
                     option = {
+                        "option_id": f"mcr{entry['mcr_mhz']}_fcr{fcr_ghz}_zone{zone_idx}",
                         "mcr_mhz": entry["mcr_mhz"],
                         "fcr_ghz": fcr_ghz,
                         "nyquist_zone": zone_idx,
@@ -97,15 +109,126 @@ def generatePartialOptions(fc, bw, mcr_converter_rates_table_path, partial_optio
 
     # Store the partial options in a json file
     storeJSON(partial_options, partial_options_path)
-    return partial_options
     
 
-def generateCompleteOptions(partial_options):
+def generateCompleteOptions(fc, bw, partial_options_path):
     complete_options = []
     
+    # Read the partial options from the json file
+    partial_options = readJSON(partial_options_path)
+    if partial_options is None:
+        return []
+    
+    # Determine the min and max frequencies of the interest BW
+    f_min_target = fc - (bw / 2)
+    f_max_target = fc + (bw / 2)
+
+    # Iterate through the partial options and generate complete options by combining them if needed   
+    while len(partial_options) > 0:
+        # Extract the first partial option and remove it from the list to avoid using it again
+        partial_option = partial_options.pop(0) 
+        
+        if partial_option["is_complete"]:
+            # Create a complete option
+            complete_option = {
+                "complete_option_id": partial_option["option_id"],
+                "partial_options": [partial_option],
+                "f_start": partial_option["f_start"],
+                "f_end": partial_option["f_end"],
+                "chans_needed": partial_option["chans_needed"],
+                "is_complete": True
+                }
+
+            # Add to the complete options list
+            complete_options.append(complete_option)
+        else:
+            # We create a list of all the combinations (useful or not)
+            partial_options_combinations = [{"partial_options_list": [partial_option], "is_complete": False}]
+            
+            # We create a list of the remaining partial options to analyse
+            partial_options_remaining = partial_options.copy()
+            total_options_checked_since_last_expansion = 0
+            
+            # While there are still partial options to analyse
+            while total_options_checked_since_last_expansion < len(partial_options_remaining) and len(partial_options_remaining) > 0:
+                # Update flag
+                total_options_checked_since_last_expansion += 1
+                
+                # Extract the current partial option 
+                current_partial_option = partial_options_remaining.pop(0) # Remove it from the list
+                
+                # Check if the current partial option is already complete, if it is, we skip it
+                if current_partial_option["is_complete"]:
+                    continue
+                
+                # Move the current partial option to the end of the list of combinations to analyse
+                partial_options_remaining.append(current_partial_option) 
+                
+                # See how the current partial option can be combined with the existing combinations to create new ones
+                for combination in partial_options_combinations:
+                    # Check if the combination is already complete, if it is, we skip it
+                    if combination["is_complete"]:
+                        continue
+                    
+                    # If the current partial option f_min is smaller than the combination.list[-1] f_max and greater than the combination.list[-1] f_min but greater than the combination.list[-2] f_max(provided it exists), then we append it to the end of the combination list and check if it is complete.
+                    if current_partial_option["f_start"] < combination["partial_options_list"][-1]["f_end"] and current_partial_option["f_end"] > combination["partial_options_list"][-1]["f_end"] and (len(combination["partial_options_list"]) == 1 or current_partial_option["f_start"] > combination["partial_options_list"][-2]["f_end"]):
+                        combination["partial_options_list"].append(current_partial_option)
+                        
+                        # Check if the combination is complete
+                        if(combination["partial_options_list"][-1]["f_end"] >= f_max_target and combination["partial_options_list"][0]["f_start"] <= f_min_target):
+                            combination["is_complete"] = True
+                        else:
+                            total_options_checked_since_last_expansion = 0 # We reset this counter since we have expanded at least one combination   
+                        
+                    # If the current partial option f_max is greater than the combination.list[0] f_min and smaller than the combination.list[0] f_max but smaller than the combination.list[1] f_min(provided it exists), then we append it to the start of the combination list and check if it is complete.
+                    elif current_partial_option["f_end"] > combination["partial_options_list"][0]["f_start"] and current_partial_option["f_start"] < combination["partial_options_list"][0]["f_start"] and (len(combination["partial_options_list"]) == 1 or current_partial_option["f_end"] < combination["partial_options_list"][1]["f_start"]):
+                        combination["partial_options_list"].insert(0, current_partial_option)
+                        
+                        # Check if the combination is complete
+                        if(combination["partial_options_list"][-1]["f_end"] >= f_max_target and combination["partial_options_list"][0]["f_start"] <= f_min_target):
+                            combination["is_complete"] = True
+                        else:
+                            total_options_checked_since_last_expansion += 1 # We increment this counter since we have not expanded this combination
+                            
+                    
+            
+            # Filter all bad combinations (not complete or with less than 8 channels)
+            correct_combinations = []
+            for combination in partial_options_combinations:
+                total_chans_needed = sum([option["chans_needed"] for option in combination["partial_options_list"]])
+                if combination["is_complete"] and total_chans_needed <= 8:
+                    correct_combinations.append(combination)
+                
+            # Append the correct combinations to the complete options list
+            for combination in correct_combinations:
+                # Create a complete option
+                complete_option = {
+                    "complete_option_id": len(complete_options),
+                    "partial_options": combination["partial_options_list"],
+                    "f_start": combination["partial_options_list"][0]["f_start"],
+                    "f_end": combination["partial_options_list"][-1]["f_end"],
+                    "chans_needed": sum([option["chans_needed"] for option in combination["partial_options_list"]]),
+                    "is_complete": True
+                    }
+
+                # Add to the complete options list
+                complete_options.append(complete_option)
+        
+         
+    storeJSON(complete_options, './assistanceJSONs/completeOptions.json')
+            
+
+
+def filter_complete_options(complete_options):
+    print("Filtering complete options to find the best one...")
     
     
-    return complete_options
+def sort_complete_options(complete_options):
+    print("Sorting complete options by number of channels needed...")
 
 # --- Example usage with your capture options ---
-capture_results = generatePartialOptions(2.1e9, 100e6, './assistanceJSONs/mcr_converter_rates_table.json', './assistanceJSONs/partialOptions.json')
+fcd = 2.1e9  # 2.1 GHz
+bw = 800e6  # 800 MHz
+#generatePartialOptions(fcd, bw, './assistanceJSONs/mcr_converter_rates_table.json', './assistanceJSONs/partialOptions.json')
+generateCompleteOptions(fcd, bw, './assistanceJSONs/partialOptions.json')
+# -------------------------------------------------
